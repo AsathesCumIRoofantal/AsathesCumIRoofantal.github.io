7f600c25cc1702f6e19f3bfe2bc94144d5737f5d
NavinKumarChahar <navinflutterdeveloper@gmail.com>
2026-05-24 08:42:32 +0530

    design web content

---
+++ lib/web_modules/web_vision/pages/web_windup_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class WebWindupController extends GetxController {
+  final totalTimeSpent = '14h 22m'.obs;
+  final modulesCompleted = 12.obs;
+  final totalInsightsGained = 450.obs;
+
+  void finalizeSession() {
+    Get.snackbar(
+      "Session Closed",
+      "Your journey state has been archived in the Sliver-Vault.",
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: const Color(0xFFD4AF37),
+      colorText: Colors.white,
+    );
+    Future.delayed(const Duration(seconds: 2), () => Get.offAllNamed('/'));
+  }
+}
