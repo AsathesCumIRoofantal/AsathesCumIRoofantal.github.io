df4f15e5e4a233cf0c69f8f0733395a35795b928
NavinKumarChahar <navinflutterdeveloper@gmail.com>
2026-05-25 12:20:03 +0530

    ready live

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Download the Flutter SDK stable branch
-git clone https://github.com -b stable --depth 1
+
 
 # Add Flutter to the temporary environment path
 export PATH="$PATH:`pwd`/flutter/bin"
